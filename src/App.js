9c84edd5f37e929dd1b000f47dffc860f2486926
Qais mohammad <zainzinc@Qaiss-iMac.local>
2019-05-01 13:47:58 +0300

    Smart-Brain

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+
+const particlesOptions =  {
+  particles: {
+    number: {
+      value: 100,
+      density : {
+        enable: true,
+        value_area: 1000,
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles
+         className='particles'
+          params= {particlesOptions}
+        />
         <Navigation/>
         <Logo/>
+        <Rank/>
         <ImageLinkForm/>
+        
         {/* {<FaceRecognition /> */} 
       </div>
     );
