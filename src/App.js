11fcd2abb687ac95b47d90fd799557071faa9f98
Qais mohammad <zainzinc@Qaiss-iMac.local>
2019-05-01 15:55:46 +0300

    init

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
+
+const app = new Clarifai.App({
+  apiKey: '8f6a85b40e5c4d6ca80aee8a93decda5'
+ });
 
 const particlesOptions =  {
   particles: {
@@ -20,6 +25,32 @@ const particlesOptions =  {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.vlaue);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click')
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b", 
+      "https://samples.clarifai.com/face-det.jpg")
+    .then(
+    function(response) {
+      console.log(response)
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
+
   render() {
     return (
       <div className="App">
@@ -30,7 +61,10 @@ class App extends Component {
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
+        <ImageLinkForm
+         onInputChange={this.onInputChange} 
+         onButtonSubmit={this.onButtonSubmit}
+        />
         
         {/* {<FaceRecognition /> */} 
       </div>
